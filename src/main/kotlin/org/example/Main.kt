819b27fc819e09ac791df71ec7ef4255baeb4bc1
p-adams <ghusiyy@gmail.com>
2022-01-15 11:25:51 -0800

    add JavaFx boilerplate

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -1,7 +1,20 @@
 package org.example
 
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+
+class RestaurantApp : Application() {
+    override fun start(stage: Stage) {
+        val restaurantApp = MainController()
+        stage.title = "Restaurant App"
+        stage.scene = Scene(StackPane(), 400.0, 400.0)
+        stage.show()
+    }
+}
 
 fun main(args: Array<String>) {
-    buildMenu()
+    Application.launch(RestaurantApp::class.java)
 }
 
---
+++ src/main/kotlin/org/example/MainController.kt
@@ -0,0 +1,4 @@
+package org.example
+// buildMenu() build menu // TODO: implement menu upload
+class MainController() {
+}
