3cf30164d6a372374cf47043170add1c5548f52b
p-adams <ghusiyy@gmail.com>
2022-02-01 01:05:10 -0800

    connect to live DB

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { Neo4jGraphQL } from "@neo4j/graphql";
 import { ApolloServer, gql } from "apollo-server";
 import neo4jDriver from "neo4j-driver";
+import "dotenv/config";
 const neo4j = neo4jDriver;
 
 const typeDefs = gql`
@@ -16,8 +17,11 @@ const typeDefs = gql`
 `;
 
 const driver = neo4j.driver(
-  "bolt://localhost:7687",
-  neo4j.auth.basic("neo4j", "password")
+  process.env.DB_ENDPOINT as string,
+  neo4j.auth.basic(
+    process.env.DB_NAME as string,
+    process.env.DB_PASSCODE as string
+  )
 );
 
 const newSchema = new Neo4jGraphQL({ typeDefs, driver });
