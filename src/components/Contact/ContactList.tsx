0f6c8ff6012239bd94137a6ca34ef890557ca96d
Paul Adams <ghusiyy@gmail.com>
2022-05-11 23:09:10 -0700

    create contact stylesheet

--- src/components/Contact/ContactList.tsx
+++ src/components/Contact/ContactList.tsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 import { mockContactListData } from "../../api/index";
-
+import "./Contact.css";
 function ContactList() {
   return (
     <article className="contact-list-wrapper">
-      contact list{" "}
-      <ul>
+      <ul className="list">
         {mockContactListData().contacts.map((el, i) => (
-          <li key={i}>
+          <li className="list-item" key={i}>
             <Link to={el.id}>{el.fullname}</Link>
           </li>
         ))}
